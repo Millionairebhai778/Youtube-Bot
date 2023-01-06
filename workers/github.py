18dbb476de777fa79a9f1a347ef46171e80e541d
Akhil Chandran <119779525+Millionairebhai778@users.noreply.github.com>
2023-01-06 11:04:55 +0530

    Update github.py

--- workers/github.py
+++ workers/github.py
@@ -16,22 +16,22 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def love(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("♂️ Github", url="https://github.com/PredatorHackerzZ")],
+        [InlineKeyboardButton("♂️ Github", url="https://t.me/kurachkanjiedukkatte/8")],
     ])
     Aww = f"""Hey <b>{message.from_user.first_name}</b>
 If you liked my project and want to be a GitHub contributor then:
-- 🗣️ You may contact at **@TeleRoid14**
+- 🗣️ You may contact at **@FILMWORLDOFFI**
 
-- 🧾 You can personal message me in Telegram **@HelpLessBoi**   
+- 🧾 You can personal message me in Telegram **@FILMWORLDOFFI**   
 
 - Star & Fork my GitHub Repo else You are Gay.\n
 
 If you liked my project and want and want to Donate then you can:
-- [PayPal](https://paypal.me/AbhishekKumarIN47) 
+- [PayPal](https://t.me/FILMWORLDOFFICIA) 
     
 **<b>{message.from_user.first_name}</b> 😁 Thanks a lot for using my bot**
 
-[@YoutubeXDBot](https://telegra.ph/file/a532f298b920e99bd58bb.jpg)
+[@FILMWORLDOFFI](https://telegra.ph/file/a532f298b920e99bd58bb.jpg)
 """      
     await message.reply_text(Aww, reply_markup=joinButton)
     raise StopPropagation
